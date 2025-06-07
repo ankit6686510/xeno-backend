d6239e18ccc2e65fb5169729ff2c8da928105202
Ankit Jha <ankit6686510@gmail.com>
2025-06-07 13:45:55 +0530

    swagger

--- server/src/docs/swagger/index.js
+++ server/src/docs/swagger/index.js
@@ -19,7 +19,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.SERVER_URL || 'https://xeno-backend-production-49a5.up.railway.app/',
+        url: process.env.SERVER_URL || 'https://xeno-backend-production-49a5.up.railway.app/api-docs/#/' ||'https://xeno-backend-production-49a5.up.railway.app/',
         description: 'Development server',
       },
     ],
